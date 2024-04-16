6d77a4038960fa0dfecfc045ac7435080ea2e87b
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-04-16 15:14:48 +0200

    refactor(Main):Change the logic of the program

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,10 @@ import com.ripadbaisor.logicalprocesses.LogicProcess;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
         LogicProcess logic = new LogicProcess();
-
-        logic.addRestaurant();
-        logic.addRestaurant();
-        logic.addRestaurant();
-        logic.addRestaurant();
-
-        logic.showRestaurants();
+        logic.setKeepAsking(true);
+        while (logic.isKeepAsking()) {
+            logic.mainMenu();
+        }
     }
 }
